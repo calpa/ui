ff067c6dcefb30aab77ce06991666bb0cddc2ed1
calpa <calpaliu@gmail.com>
2022-12-19 11:30:30 +0800

    feat: create Buttons

---
+++ src/components/Button/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Button } from '@mui/material'
+
+const CustomButton = (props) => {
+    return (
+        <Button
+            {...props}
+        />
+    );
+}
+
+export default CustomButton
+
+CustomButton.defaultProps = {
+    variant: 'contained',
+    color: 'primary',
+    onClick: undefined,
+}
+
+CustomButton.propTypes = {
+    variant: PropTypes.oneOf(['contained', 'outlined', 'text']),
+    color: PropTypes.oneOf(['inherit', 'primary', 'secondary', 'error', 'info', 'success', 'warning']),
+    onClick: PropTypes.func,
+}
