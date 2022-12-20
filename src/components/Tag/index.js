a17777fd2810d78aff054ecd1ea02b84a834f09f
calpa <calpaliu@gmail.com>
2022-12-20 14:58:52 +0800

    feat: create Tag component

---
+++ src/components/Tag/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css'
+
+const Tag = ({ tag, children }) => (
+    <a href={`/tag/${tag}`} className="tag">
+        {tag}
+    </a>
+);
+
+Tag.propTypes = {
+    tag: PropTypes.string,
+    children: PropTypes.node,
+};
+
+Tag.defaultProps = {
+    tag: '',
+};
+
+export default Tag;
---
+++ src/components/Tag/index.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Tag from ".";
+
+export default {
+    title: "Components/Tag",
+    component: Tag,
+};
+
+const Template = (args) => <Tag {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    tag: "JavaScript",
+    children: "JavaScript"
+}
