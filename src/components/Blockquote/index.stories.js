26a7a64434bc07106539741bae55453260d9fb91
calpa <calpaliu@gmail.com>
2022-12-20 18:31:28 +0800

    feat: create blockquote component

---
+++ src/components/Blockquote/index.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Blockquote from '.';
+
+export default {
+    title: 'Components/Blockquote',
+    component: Blockquote,
+};
+
+const Template = (args) => <Blockquote {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}
