f2d7e028bf248ddbde06ebe384009981e96aecc7
calpa <calpaliu@gmail.com>
2022-12-20 12:08:08 +0800

    feat: add theme story

---
+++ src/stories/theme.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Grid } from "@mui/material"
+
+export default {
+    title: `Theme`,
+}
+
+const Item = (props) => (
+    <Grid
+        item
+        sx={{
+            backgroundColor: props.backgroundColor,
+            padding: `10px`,
+            width: `100px`
+        }}
+    >
+        {props.children}
+    </Grid>
+)
+
+const colors = ['primary', 'secondary', 'error', 'info', 'success', 'warning']
+
+const Template = () => {
+    return (
+        <Grid container>
+            {
+                colors.map(
+                    (color) => (
+                        <Item key={color} backgroundColor={`${color}.main`}>
+                            {color}
+                        </Item>
+                    )
+                )
+            }
+        </Grid>
+    )
+}
+
+export const Default = Template.bind({})
+
--- src/themes/light.theme.js
+++ src/themes/light.theme.js
@@ -1,7 +1,6 @@
 // src/themes/dark.theme.js
 
 import { createTheme } from "@mui/material";
-import { blue, blueGrey, cyan, pink } from "@mui/material/colors";
 import components from "./components";
 
 export const lightTheme = createTheme({
@@ -9,14 +8,16 @@ export const lightTheme = createTheme({
     palette: {
         mode: "light",
         primary: {
-            main: blue["A200"],
+            main: `#81C7D4`
         },
         secondary: {
-            main: blue["A400"],
+            main: `#faf7a1`
         },
-        background: {
-            // default: blueGrey["800"],
-            // paper: blueGrey["700"],
+        error: {
+            main: `#CB1B45`
         },
+        success: {
+            main: `#BEC23F`
+        }
     },
 });
