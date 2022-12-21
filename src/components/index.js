7937cab416d163f0bd104ea80ecf1c54d9e1994b
calpa <calpaliu@gmail.com>
2022-12-21 08:25:25 +0800

    refactor: provide entry point

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Blockquote from "./Blockquote";
+import Button from "./Button";
+import Footer from "./Footer";
+import GithubCorners from "./GithubCorners";
+import NavBar from "./NavBar";
+import SignIn from "./SignIn";
+import Tag from "./Tag";
+
+export default {
+  Blockquote,
+  Button,
+  Footer,
+  GithubCorners,
+  NavBar,
+  SignIn,
+  Tag,
+};
