8527ccfb3a356d26f49e4ce7349dacbf1317d5d5
Calpa <calpaliu@gmail.com>
2022-12-30 20:41:28 +0800

    fix: Link anchor check

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -2,15 +2,23 @@ import PropTypes from "prop-types";
 
 import "./index.css";
 
-const Tag = ({ tag, children }) => (
-  <a href={`/tag/${tag}`} className="tag">
-    {tag}
-  </a>
-);
+const Tag = ({ tag, children, Link = "a" }) => {
+  // Check if the Link prop is an "a" tag
+  const isAnchor = Link === "a"; // Will be true if Link is an "a" tag
+
+  // Use the appropriate prop based on the value of the Link prop
+  const linkProps = {
+    [isAnchor ? "href" : "to"]: `/tag/${tag}`,
+    className: "tag",
+  };
+
+  return <Link {...linkProps}>{tag}</Link>;
+};
 
 Tag.propTypes = {
   tag: PropTypes.string,
   children: PropTypes.node,
+  Link: PropTypes.elementType, // This specifies that the Link prop is a component
 };
 
 Tag.defaultProps = {
