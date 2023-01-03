5d2e852a8e1336315a92b46841db0bee325dc5dd
Calpa <calpaliu@gmail.com>
2023-01-03 21:16:43 +0800

    fix: export list and listitem

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,5 @@ export { default as NavBar } from "./NavBar";
 export { default as SignIn } from "./SignIn";
 export { default as Tag } from "./Tag";
 export { default as JapanMap } from "./JapanMap";
+export { default as List } from "./List";
+export { default as ListItem } from "./ListItem";
